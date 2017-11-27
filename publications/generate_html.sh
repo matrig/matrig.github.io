5013eb544167a417736a1fbdc2d2314c6c855e15
Mattia Rigotti <matrig@hotmail.com>
2017-11-26 20:03:44 -0500

    Add preprint and code links to articles

--- publications/generate_html.sh
+++ publications/generate_html.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #@date Wed Oct 26 23:18:12 EDT 2016
 #@author Mattia Rigotti
-line_parser.py -i articles.txt -t template_articles
-mv output articles.html
+line_parser.py -i articles.txt -t template_articles -o articles.html
 
-line_parser.py -i abstracts.txt -t template_abstracts
-mv output abstracts.html
+line_parser.py -i abstracts.txt -t template_abstracts -o abstracts.html
